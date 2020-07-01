ea876458980c0bccf48034ba0f474e7242b23e8d
Suraj Singh <suraj1998@gmail.com>
2020-07-01 21:22:43 +0530

    primitive acquire and release functions for client

--- internal/lockclient/simple_client.go
+++ internal/lockclient/simple_client.go
@@ -1,6 +1,12 @@
 package lockclient
 
-import "github.com/GoPlayAndFun/LocKey/internal/lockservice"
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/GoPlayAndFun/LocKey/internal/lockservice"
+)
 
 var _ Config = (*SimpleConfig)(nil)
 
@@ -24,18 +30,60 @@ var _ Client = (*SimpleClient)(nil)
 
 // SimpleClient implements Client, the lockclient for Lockey.
 type SimpleClient struct {
+	Id     uint8
+	config *SimpleConfig
 }
 
 // Acquire makes a HTTP call to the lockserver and acquires the lock.
 // The errors invloved may be due the HTTP errors or the lockservice errors.
 func (sc *SimpleClient) Acquire(d lockservice.Descriptors) error {
-	panic("TODO")
+	baseUrl := sc.config.IPAddr + ":" + sc.config.PortAddr
+	endPoint := baseUrl + "/acquire"
+
+	var jsonStr = []byte(`{"FileID":` + string(d) + "}")
+	req, err := http.NewRequest("POST", endPoint, bytes.NewBuffer(jsonStr))
+	req.Header.set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if string(body) == "lock acquired" {
+		return nil
+	}
+
 }
 
 // Release makes a HTTP call to the lockserver and acquires the lock.
 // The errors invloved may be due the HTTP errors or the lockservice errors.
 func (sc *SimpleClient) Release(d lockservice.Descriptors) error {
-	panic("TODO")
+	baseUrl := sc.config.IPAddr + ":" + sc.config.PortAddr
+	endPoint := baseUrl + "/release"
+
+	var jsonStr = []byte(`{"FileID":` + string(d) + "}")
+	req, err := http.NewRequest("POST", endPoint, bytes.NewBuffer(jsonStr))
+	req.Header.set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if string(body) == "lock released" {
+		return nil
+	}
+
 }
 
 // StartService starts the lockservice Lockey.
