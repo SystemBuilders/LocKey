de42353e1726be2a13ba2dbb35755007ce365e62
Sumukha Pk <sumukhapk46@gmail.com>
2020-11-08 08:46:40 +0530

    Update internal/lockclient/errors.go

--- internal/lockclient/errors.go
+++ internal/lockclient/errors.go
@@ -8,6 +8,6 @@ func (e Error) Error() string { return string(e) }
 // Constant errors.
 // Rule of thumb, all errors start with a small letter and end with no full stop.
 const (
-	ErrSessionInexistent = Error("the session related to this process doesn't exist")
+	ErrSessionNonExistent = Error("the session related to this process doesn't exist")
 	ErrSessionExpired    = Error("session expired")
 )
